9d7171e885a8fb45a54ae7a167b15741aa744938
Sean Yen (UCSD work laptop) <ussa.say@gmail.com>
2025-05-01 15:37:55 -0700

    Updates for SDCTF 2025
    - 2024 -> 2025
    - update dates (time may be incorrect)
    - add links to all challenge archives
    - link to cyber.acmucsd.com :)

--- src/public/components/Config.tsx
+++ src/public/components/Config.tsx
@@ -1,5 +1,10 @@
 export const DISCORD_INVITE = "https://discord.gg/GpC6YFrf4h";
-export const CHALLENGES_URL = "https://github.com/acmucsd/sdctf-2023";
+export const CHALLENGE_ARCHIVE_URLS: ChallengeArchive[] = [
+  { label: "See 2024 challenges", url: "https://github.com/acmucsd/sdctf-2024" },
+  { label: "See 2023 challenges", url: "https://github.com/acmucsd/sdctf-2023" },
+  { label: "See 2022 challenges", url: "https://github.com/acmucsd/sdctf-2022" },
+  { label: "See 2021 challenges", url: "https://github.com/acmucsd/sdctf-2021" },
+];
 export const SIGNUP_URL = "https://ctf.sdc.tf";
 export const CONTACT_LINK = "mailto:cyber@acmucsd.org";
 export const SPONSOR_PACKET = "https://drive.google.com/file/d/12DBZkeSBVT4MwYjSwcVF2uct84nouBPP/view";
@@ -7,3 +12,9 @@ export const SPONSOR_PACKET = "https://drive.google.com/file/d/12DBZkeSBVT4MwYjS
 export const FACEBOOK = "https://www.facebook.com/cyberucsd/";
 export const LINKEDIN = "https://www.linkedin.com/company/cyber-ucsd";
 export const TWITTER = "https://twitter.com/cyberucsd";
+export const WEBSITE = "https://cyber.acmucsd.com/";
+
+export type ChallengeArchive = {
+  label: string;
+  url: string;
+};
